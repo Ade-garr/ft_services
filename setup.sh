338cef583415ef2e5ff9749f63c481785e947dd8
ade-garr <ade-garr@student.42.fr>
2021-01-08 00:23:03 +0100

    08/01/21_00h22

--- setup.sh
+++ setup.sh
@@ -2,9 +2,16 @@ service nginx stop
 minikube delete
 minikube start
 
+# paramétrage du docker
+eval $(minikube docker-env)
+
 # MetalLB
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
     # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f metalLB.yaml 
+
+
+# obtenir l'IP de minikube
+kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p
